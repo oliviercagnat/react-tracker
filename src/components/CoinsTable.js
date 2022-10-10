954a1f6f433e9702878f0d53cbcc349c14e15d6f
oliviercagnat <oliviercagnat@gmail.com>
2022-10-10 14:09:07 +0200

    finish table

--- src/components/CoinsTable.js
+++ src/components/CoinsTable.js
@@ -18,11 +18,14 @@ import {
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
+import { numberWithCommas } from './Banner/Caroussel';
+import { Pagination } from '@material-ui/lab';
 
 const CoinsTable = () => {
   const [coins, setCoins] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [search, setSearch] = useState();
+  const [search, setSearch] = useState('');
+  const [page, setPage] = useState(1);
   const { currency, symbol } = CurrencyState();
   const history = useHistory();
 
@@ -37,6 +40,7 @@ const CoinsTable = () => {
   // Get loaded again everytime the currency changes
   useEffect(() => {
     fetchCoins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [currency]);
 
   const darkTheme = createTheme({
@@ -67,9 +71,7 @@ const CoinsTable = () => {
   const MUIclasses = useStyles();
 
   const handleSearch = () => {
-    return coins.filter((coin) => coin.name.toLowerCase().includes(search));
-    // ||
-    // coin.symbol.toLowerCase.includes(search));
+    return coins.filter((coin) => coin.name.toLowerCase().includes(search) || coin.symbol.toLowerCase().includes(search));
   };
 
   return (
@@ -100,19 +102,47 @@ const CoinsTable = () => {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {handleSearch().map((row) => {
-                  const profit = row.price_change_percentage_24h > 0;
-
-                  return (
-                    <TableRow onClick={() => history.push(`/coins/${row.id}`)} className={MUIclasses.row} key={row.name}>
-                      <TableCell component="th" scope="row" style={{ display: 'flex', gap: 15 }}></TableCell>
-                    </TableRow>
-                  );
-                })}
+                {/* Only display from 0 to 10 if page 1, 10 to 20 if page 2 etc. */}
+                {handleSearch()
+                  .slice((page - 1) * 10, (page - 1) * 10 + 10)
+                  .map((row) => {
+                    console.log();
+                    const profit = row.price_change_percentage_24h > 0;
+                    return (
+                      <TableRow onClick={() => history.push(`/coins/${row.id}`)} className={MUIclasses.row} key={row.name}>
+                        <TableCell component="th" scope="row" style={{ display: 'flex', gap: 15 }}>
+                          <img src={row?.image} alt={row.name} height="50" style={{ marginBottom: 10 }} />
+                          <div style={{ display: 'flex', flexDirection: 'column' }}>
+                            <span style={{ textTransform: 'uppercase', fontSize: 22 }}>{row.symbol}</span>
+                            <span style={{ color: 'darkgrey' }}>{row.name}</span>
+                          </div>
+                        </TableCell>
+                        <TableCell align="right">
+                          {symbol} {numberWithCommas(row.current_price.toFixed(2))}
+                        </TableCell>
+                        <TableCell align="right" style={{ color: profit > 0 ? 'rgb(14, 203, 129' : 'red', fontWeight: 500 }}>
+                          {profit && '+'}
+                          {row.price_change_percentage_24h.toFixed(2)}%
+                        </TableCell>
+                        <TableCell align="right">
+                          {symbol} {numberWithCommas(row.market_cap.toString().slice(0, -6))}M
+                        </TableCell>
+                      </TableRow>
+                    );
+                  })}
               </TableBody>
             </Table>
           )}
         </TableContainer>
+        <Pagination
+          style={{ padding: 20, width: '100%', display: 'flex', justifyContent: 'center' }}
+          classes={{ ul: MUIclasses.pagination }}
+          count={(handleSearch()?.length / 10).toFixed(0)}
+          onChange={(_, value) => {
+            setPage(value);
+            window.scroll(0, 450);
+          }}
+        />
       </Container>
     </ThemeProvider>
   );
